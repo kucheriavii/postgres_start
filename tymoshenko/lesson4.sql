b830bed7d6c8201fee2d5ce3fd153226da9d6289
kucheriavii <kucheriavii@gmail.com>
2023-12-09 19:18:58 +0200

    just trying

--- tymoshenko/lesson4.sql
+++ tymoshenko/lesson4.sql
@@ -1,7 +1,13 @@
 -- SELECT * FROM salaries;
 
-SELECT year
-, exp_level
-, emp_type
-FROM salaries
-LIMIT 10;
+-- SELECT year
+-- , exp_level
+-- , emp_type
+-- FROM salaries
+-- LIMIT 10;
+
+SELECT DISTINCT emp_type,
+CASE emp_type
+    WHEN 'PT' then 'FITTU'
+END
+FROM salaries;
