f87c6ab96fdbb48120b5c289eb249049b566a2ab
r.ostapchuk <kucheriavii@gmail.com>
2023-12-30 10:43:56 +0200

    lesson 4

--- tymoshenko/lesson4.sql
+++ tymoshenko/lesson4.sql
@@ -1,5 +1,21 @@
-SELECT * FROM salaries;
+-- SELECT * FROM salaries;
+
+-- SELECT year
+--     , exp_level as level
+--     , emp_type  as type
+-- FROM salaries
+-- WHERE 
+--     year != 2022
+--     AND exp_level = 'SE'
+-- ORDER BY year ASC
+-- LIMIT 20;
 
 
+-- Top 5 salaries 2023
 
-
+SELECT year, exp_level, job_title, salary_in_usd
+FROM salaries
+WHERE year = 2023 
+    AND job_title = 'Data Scientist'
+ORDER BY salary_in_usd DESC
+LIMIT 5
