fc69d2d0a845f50a5e80bca04fb532e7a53cf5d6
r.ostapchuk <kucheriavii@gmail.com>
2023-12-30 11:03:59 +0200

    lesson 5

--- tymoshenko/lesson5.sql
+++ tymoshenko/lesson5.sql
@@ -1,24 +1,34 @@
 -- SELECT
---     COUNT(*) as cnt_all
---     , COUNT(exp_level) AS cnt_level
+--     1 AS nmb_1
+--     , 2 AS nmb_2
+--     , 5*55 as nmb_3
+
+-- SELECT 
+--     count(*) as cnt_all
+--     , count(exp_level) as cnt_level
+--     , count(DISTINCT exp_level)
 -- FROM salaries
--- LIMIT 10;
+-- LIMIT 10
 
 -- SELECT
---     AVG(salary_in_usd) AS salary_avg,
---     MIN(salary_in_usd) AS salary_min,
---     MAX(salary_in_usd) AS salary_max
+--     ROUND(AVG(salary_in_usd),0) as salary_avg
+--     , MIN(salary_in_usd) as salary_min
+--     , MAX(salary_in_usd) as salary_max
 -- FROM salaries
--- WHERE year = '2023'
--- LIMIT 10;
-
-SELECT year
-        , exp_level
-        , salary_in_usd
-        , salary_in_usd * 38
-        -- 'SE' - senior
-        , CASE
-            WHEN exp_level = 'SE' THEN 'Senior'
-            WHEN exp_level = 'MI' THEN 'Middle'
-            ELSE 'Others' END
-FROM salaries;
+-- WHERE year = 2023
+-- LIMIT 10
+    
+SELECT
+    year
+    , exp_level
+    , salary_in_usd
+    , salary_in_usd*38 as salary_in_uah
+    , CASE 
+        WHEN exp_level = 'SE'
+        THEN 'senior'
+        WHEN exp_level = 'MI'
+        THEN 'middle'
+        ELSE 'others'
+    END as fill_level
+FROM salaries
+LIMIT 100
