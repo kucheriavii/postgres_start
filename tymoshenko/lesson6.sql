7632c7f2015c8f25b269162401aba5680f6d307c
r.ostapchuk <kucheriavii@gmail.com>
2023-12-30 13:50:16 +0200

    2 task slowed

--- tymoshenko/lesson6.sql
+++ tymoshenko/lesson6.sql
@@ -9,14 +9,22 @@
 --     AND job_title ='ML Engineer'
 -- ORDER BY salary_in_usd ASC
 
--- country with the lowest salary for person
+-- country with the lowest salary for person in Data Scientisy in 2023
 -- *************************************************
 SELECT company_location 
 FROM salaries
-WHERE salary_in_usd = (SELECT MIN(salary_in_usd) FROM salaries);
+WHERE job_title = 'Data Scientist' 
+AND salary_in_usd = (SELECT MIN(salary_in_usd) 
+                        FROM salaries 
+                        WHERE job_title = 'Data Scientist' 
+                        AND year = 2023) ;
+
 
 SELECT company_location
 FROM salaries
+WHERE 
+    job_title = 'Data Scientist'
+    AND year = 2023
 ORDER BY salary_in_usd ASC
-LIMIT 1
+LIMIT 1;
 
