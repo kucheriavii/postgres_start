9b4f0bd31b865218601899f230022963fba98a2c
r.ostapchuk <kucheriavii@gmail.com>
2024-01-06 17:42:20 +0200

    rank || danceRank()

--- tymoshenko/salaries.sql
+++ tymoshenko/salaries.sql
@@ -14,8 +14,28 @@
 
 --вивести працівників середя зп яких вища за сер зп професії де вони працюють
 
-with sal as (select 
-	job_title,
-	avg(salary_in_usd) 
-from salaries  
-group by 1)
+--with av_sal as (
+--select 
+--	job_title,
+--	avg(salary_in_usd) as averege
+--from salaries  
+--where year = 2023
+--group by 1)
+--
+--select s.job_title , s.salary_in_usd, a.averege  
+--from salaries s
+--left join av_sal a on s.job_title = a.job_title
+--where s.salary_in_usd > a.averege and year = 2023 
+
+with cte as (
+	select
+		job_title
+		,salary_in_usd		
+		,AVG(salary_in_usd) over(partition by job_title) as avg_salary
+	from  salaries s 
+	where year = 2023
+)
+
+select  * 
+from cte
+where salary_in_usd > avg_salary
